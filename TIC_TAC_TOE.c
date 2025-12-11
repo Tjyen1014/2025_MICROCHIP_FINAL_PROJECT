4f764e13c948e588a9b157fd474d4bab26b0f245
Tjyen1014 <tanjingyen1014@gmail.com>
2025-12-11 21:07:31 +0800

    delete struct

--- TIC_TAC_TOE.c
+++ TIC_TAC_TOE.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
-
-typedef struct {
-    /***
-        board: 0 is O, 1 is X, -1 is non-fill
-        0,1,2
-        3,4,5
-        6,7,8
-        curplayer: 0 is O, 1 is X (who lead board change this time)
-        detwinner: 0 no winner, 1 has winner
-        success: the move legal or not
-    ***/
-    int BOARD[9];
-	int CURPLAYER;
-	int DETWINNER;
-	int SUCCESS;
-} TTT_OUTPUT_TABLE;
+#include "INTERFACE.h"
 
 int PQ[2][3];//playerQueue
 int pt[2];
